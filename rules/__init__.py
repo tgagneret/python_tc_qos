849710291e9f53fd5f5f1c836bd4f2e72b61ccd6
Anthony Ruhier <anthony.ruhier@gmail.com>
2014-12-05 01:37:55 +0100

    Add openvpn and torrents
    Change all marks, old numerotation was abusive

--- rules/__init__.py
+++ rules/__init__.py
@@ -7,10 +7,10 @@ from rules import clients, servers
 
 def apply_qos():
     # Creating the HTB root qdisc
-    tools.qdisc_add(PUBLIC_IF, "1:", "htb", default=1900)
+    tools.qdisc_add(PUBLIC_IF, "1:", "htb", default=1500)
     # Creating the main branch (htb)
     tools.class_add(PUBLIC_IF, parent="1:0", classid="1:1", rate=UPLOAD,
                     ceil=UPLOAD)
-    tools.qdisc_add(LAN_IF, "1:", "htb", default=1900)
+    tools.qdisc_add(LAN_IF, "1:", "htb", default=1500)
     clients.apply_qos()
     servers.apply_qos()
