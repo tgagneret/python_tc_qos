65d49394bd56d73a3a697927ccb8c683ec44f2f1
Anthony Ruhier <anthony.ruhier@gmail.com>
2015-01-24 22:37:12 +0100

    Change default class

--- rules/__init__.py
+++ rules/__init__.py
@@ -8,7 +8,7 @@ from rules import download
 def apply_qos():
     openvpn_if = INTERFACES["openvpn"]
     # Creating the HTB root qdisc
-    tools.qdisc_add(openvpn_if, "1:", "htb", default=1200)
+    tools.qdisc_add(openvpn_if, "1:", "htb", default=1500)
     # Creating the main branch (htb)
     tools.class_add(openvpn_if, parent="1:0", classid="1:1", rate=DOWNLOAD,
                     ceil=DOWNLOAD, burst=DOWNLOAD/8)
