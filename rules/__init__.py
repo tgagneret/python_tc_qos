3c4ff05e49d81e7a4f42d9e6ed18702f9087ceda
Anthony Ruhier <anthony.ruhier@gmail.com>
2015-01-24 22:08:43 +0100

    Fix incorrect values in main class

--- rules/__init__.py
+++ rules/__init__.py
@@ -4,6 +4,8 @@ import tools
 from config import PUBLIC_IF, LAN_IF, UPLOAD, DOWNLOAD
 from rules import clients, servers
 
+burst_formula = lambda rate: 0.5 * rate/8
+
 
 def apply_qos():
     # Creating the HTB root qdisc
@@ -13,6 +15,6 @@ def apply_qos():
                     ceil=UPLOAD, burst=UPLOAD/8)
     tools.qdisc_add(LAN_IF, "1:", "htb", default=1500)
     tools.class_add(LAN_IF, parent="1:0", classid="1:1", rate=DOWNLOAD,
-                    ceil=DOWNLOAD, burst=UPLOAD/8)
+                    ceil=DOWNLOAD, burst=burst_formula(DOWNLOAD)*3)
     clients.apply_qos()
     servers.apply_qos()
